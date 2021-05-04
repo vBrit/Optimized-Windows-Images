379cadf13884931870f242faffd987c4f143ec52
Karl Newick <karl.newick@wei.com>
2021-05-04 15:40:30 -0400

    Start of project

--- scripts/install-vm-tools.cmd
+++ scripts/install-vm-tools.cmd
@@ -1,3 +1,3 @@
 @rem Silent mode, basic UI, no reboot
-e:\setup64 /s /v "/qb REBOOT=R"
+e:\setup64.exe /s /v" /qb REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs,SVGA,VSS,AppDefense,NetworkIntrospection"
 
---
+++ scripts/install_net_framework35.cmd
@@ -0,0 +1,1 @@
+DISM /Online /Enable-Feature /FeatureName:NetFx3 /All /LimitAccess /Source:D:\sources\sxs
