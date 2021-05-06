55289c6c972d28517089c02462d0bfdb4f8a0f4c
Karl Newick <karl.newick@wei.com>
2021-05-06 13:14:24 -0400

    Resolved VMtools issue

---
+++ scripts/Start-FirstSteps.ps1
@@ -0,0 +1,49 @@
+<#
+.FUNCTIONALITY
+First steps after GUI launches on new Win assets built by packer/autonunattend.xml process
+.SYNOPSIS
+Change log
+July 23, 2020
+-Initial version
+July 25, 2020
+-Removed WinRM enablement
+July 27, 2020
+-Amended to support Win 10
+.DESCRIPTION
+Author oreynolds@gmail.com
+.EXAMPLE
+./Start-FirstSteps.ps1
+.NOTES
+.Link
+https://github.com/getvpro/Build-Packer
+#>
+
+write-host "Running first steps after GUI logon" -ForegroundColor Cyan
+
+IF (Get-process "servermanager" -ErrorAction SilentlyContinue) {
+
+    Stop-Process -name servermanager -Force
+    New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" –Force
+}
+
+### Disable network discovery
+write-host "Disable network discovery and open RDP" -ForegroundColor Cyan
+reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff /f
+netsh advfirewall firewall set rule group="Network Discovery" new enable=No
+
+### Open RDP
+netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+# Set Temp Variable using PowerShell
+write-host "Setting temp folder" -ForegroundColor Cyan
+
+$TempFolder = "C:\TEMP"
+New-Item -ItemType Directory -Force -Path $TempFolder
+[Environment]::SetEnvironmentVariable("TEMP", $TempFolder, [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("TMP", $TempFolder, [EnvironmentVariableTarget]::Machine)
+[Environment]::SetEnvironmentVariable("TEMP", $TempFolder, [EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("TMP", $TempFolder, [EnvironmentVariableTarget]::User)
+
+write-host "Script completed! Moving to next step after 5 second pause" -ForegroundColor Cyan
+start-sleep -s 5
--- scripts/install-vm-tools.cmd
+++
@@ -1,3 +0,0 @@
-@rem Silent mode, basic UI, no reboot
-e:\setup64.exe /s /v" /qb REBOOT=R ADDLOCAL=ALL REMOVE=Hgfs,SVGA,VSS,AppDefense,NetworkIntrospection"
-
